5ce9d9bf72d7d99ef24a26b7a2aa5fdefdec8732
dorian smiley <doriansmiley@yahoo.com>
2019-01-13 15:57:38 -0800

    start block object

--- lib/Block.d.ts
+++ lib/Block.d.ts
@@ -1,5 +1,12 @@
-/**
- * Created by dsmiley on 1/13/19.
- */
 export declare class Block {
+    private index;
+    private timestamp;
+    private hash;
+    private previousHash;
+    private data;
+    private nonce;
+    private crypto;
+    private algorithm;
+    constructor(index: number, timestamp: number, previousHash: string, data: string, crypto: any, algorithm?: string);
+    static calculateHash(block: Block, crypto: any, algorithm?: string): string;
 }
--- src/Block.ts
+++ src/Block.ts
@@ -2,5 +2,39 @@
  * Created by dsmiley on 1/13/19.
  */
 export class Block {
-    
+    private index: number;
+    private timestamp: number;
+    private hash: string;
+    private previousHash: string;
+    private data: string;
+    private nonce: number;
+    private crypto: any;
+    private algorithm: string;
+
+    constructor (index: number, timestamp: number, previousHash: string, data: string, crypto:any, algorithm: string = 'sha256'){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.nonce = 0;
+        this.crypto = crypto;
+        this.algorithm = algorithm;
+        this.hash = Block.calculateHash(this, this.crypto, this.algorithm);
+    }
+
+    public static calculateHash(block: Block, crypto:any, algorithm: string = 'sha256'): string {
+        if (block != null) {
+            let digest: string = null;
+            try {
+                digest = crypto.createHash('sha256').digest('hex');
+            } catch (e) {
+                // log error to std out
+                console.error(e);
+                return null;
+            }
+
+            return digest;
+        }
+        return null;
+    }
 }
