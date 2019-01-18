aa6ef702800f3931ecaa93d4f631ded154ea5e20
dorian smiley <doriansmiley@yahoo.com>
2019-01-17 21:52:03 -0800

    code out test classes

--- src/ECPair.ts
+++ src/ECPair.ts
@@ -1,9 +1,6 @@
 /**
  * Created by dsmiley on 1/17/19.
  */
-import bip39 from 'bip39';
-import bitcoin from 'bitcoinjs-lib';
-import * as crypto from 'crypto';
 import {Wallet, Network} from './Wallet'
 const ecc = require('tiny-secp256k1')
 const wif = require('wif')
@@ -11,15 +8,21 @@ const types = require('./types')
 const randomBytes = require('randombytes')
 const typeforce = require('typeforce')
 
+export type ECPairOptions = {compressed:boolean, network:Network};
+
 export class ECPair {
 
-    private _privateKey: string;
-    private _publicKey: string;
+    private _privateKey:string;
+    private _publicKey:string;
     private _network: Network;
-    private _compressed: boolean;
+    private _compressed:boolean;
+    private readonly isOptions = typeforce.maybe(typeforce.compile({
+        compressed: types.maybe(types.Boolean),
+        network: types.maybe(types.Network)
+    }))
 
-    constructor (d, Q, options) {
-        options = options || {}
+    constructor(d, Q, options:ECPairOptions) {
+        options = options || {compressed:false, network:Wallet.DEFAULT_NETWORK}
 
         this._compressed = options.compressed === undefined ? true : options.compressed
         this._network = options.network || Wallet.DEFAULT_NETWORK
@@ -28,7 +31,7 @@ export class ECPair {
         this._publicKey = null
         if (Q) this._publicKey = ecc.pointCompress(Q, this._compressed)
     }
-    
+
     get network():Network {
         return this._network;
     }
@@ -36,22 +39,76 @@ export class ECPair {
     get privateKey():string {
         return this._privateKey;
     }
+
     get publicKey():string {
         if (!this._publicKey) this._publicKey = ecc.pointFromScalar(this._privateKey, this._compressed)
         return this._privateKey
     }
 
-    public toWIF(): string {
+    public toWIF():string {
         if (!this.privateKey) throw new Error('Missing private key')
         return wif.encode(this.network.wif, this.privateKey, this._compressed)
     }
 
-    public sign(hash: string): string {
+    public sign(hash:string):string {
         if (!this.privateKey) throw new Error('Missing private key')
         return ecc.sign(hash, this.privateKey)
     }
 
-    public verify(hash: string, signature: string) {
+    public verify(hash:string, signature:string) {
         return ecc.verify(hash, this.privateKey, signature)
+    }
+
+    public fromPrivateKey(buffer:Buffer, options:ECPairOptions):ECPair {
+        typeforce(types.Buffer256bit, buffer)
+        if (!ecc.isPrivate(buffer)) throw new TypeError('Private key not in range [1, n)')
+        typeforce(this.isOptions, options)
+
+        return new ECPair(buffer, null, options)
+    }
+
+    public fromPublicKey(buffer:Buffer, options:ECPairOptions):ECPair {
+        typeforce(ecc.isPoint, buffer)
+        typeforce(this.isOptions, options)
+        return new ECPair(null, buffer, options)
+    }
+
+    public fromWIF(wiffy:string, network:any) {
+        const decoded = wif.decode(wiffy)
+        const version = decoded.version
+
+        // list of networks?
+        if (types.Array(network)) {
+            network = network.filter(function (x) {
+                return version === x.wif
+            }).pop()
+
+            if (!network) throw new Error('Unknown network version')
+
+            // otherwise, assume a network object (or default to bitcoin)
+        } else {
+            network = network || Wallet.DEFAULT_NETWORK
+
+            if (version !== network.wif) throw new Error('Invalid network version')
+        }
+
+        return this.fromPrivateKey(decoded.privateKey, {
+            compressed: decoded.compressed,
+            network: network
+        })
+    }
+
+    public makeRandom(options):ECPair {
+        typeforce(this.isOptions, options)
+        options = options || {}
+        const rng = options.rng || randomBytes
+
+        let d
+        do {
+            d = rng(32)
+            typeforce(types.Buffer256bit, d)
+        } while (!ecc.isPrivate(d))
+
+        return this.fromPrivateKey(d, options)
     }
 }
--- src/Wallet.ts
+++ src/Wallet.ts
@@ -1,9 +1,9 @@
 /**
  * Created by dsmiley on 1/17/19.
  */
-import bip39 from 'bip39';
-import bitcoin from 'bitcoinjs-lib';
-import {ECPair} from './ECPair'
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');
+import {ECPair, ECPairOptions} from './ECPair'
 import * as crypto from 'crypto';
 const ecc = require('tiny-secp256k1')
 const wif = require('wif')
@@ -12,103 +12,152 @@ const randomBytes = require('randombytes')
 const typeforce = require('typeforce')
 
 export type Network = {
-    messagePrefix: string, 
-    bech32: string,
-    bip32: {
-        public: string,
-        private: string, 
+    messagePrefix:string,
+    bech32:string,
+    bip32:{
+        public:number,
+        private:number,
     },
-    pubKeyHash: number,
-    scriptHash: number,
-    wif: number,
+    pubKeyHash:number,
+    scriptHash:number,
+    wif:number,
+    name:string
 }
 
 export class Wallet {
 
     private _password:string = null;
     private _wif:string = null;
+    private _name:string = null;
+    private _address:string = null;
+    private _network:Network = null;
+    private _utxos:Array<any> = [];
 
     public static DEFAULT_ENCRYPTION = 'aes-256-cbc';
     public static DEFAULT_PATH = "m/44'/0'/0'/0/0";
     public static DEFAULT_DB = "wallets";
     // TODO: get valid values for bitidme networks
-    public static DEFAULT_NETWORK = {
-        bitcoin: {
-            messagePrefix: '\x18Bitcoin Signed Message:\n',
-            bech32: 'bc',
-            bip32: {
-                public: 0x0488b21e,
-                private: 0x0488ade4
-            },
-            pubKeyHash: 0x00,
-            scriptHash: 0x05,
-            wif: 0x80
+    public static DEFAULT_NETWORK_TEST:Network = {
+        messagePrefix: '\x18Bitcoin Signed Message:\n',
+        bech32: 'tb',
+        bip32: {
+            public: 0x043587cf,
+            private: 0x04358394
         },
-        testnet: {
-            messagePrefix: '\x18Bitcoin Signed Message:\n',
-            bech32: 'tb',
-            bip32: {
-                public: 0x043587cf,
-                private: 0x04358394
+        pubKeyHash: 0x6f,
+        scriptHash: 0xc4,
+        wif: 0xef,
+        name: 'deaultTest'
+    };
+    public static DEFAULT_NETWORK:Network = {
+        messagePrefix: '\x18Bitcoin Signed Message:\n',
+        bech32: 'bc',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0x00,
+        scriptHash: 0x05,
+        wif: 0x80,
+        name: 'default'
+    };
+    public static DEFAULTS = {
+        Bitcoin: {
+            Decimals: 8,
+            Satoshis: 100000000,
+        },
+        Transactions: {
+            AverageBytes: 255
+        },
+        Endpoints: {
+            EllipticFees: 'https://someURL'
+        },
+        ReturnValues: {
+            TransactionSubmitted: 'Transaction Submitted',
+            NoFreeOutputs: 'No free outputs to spend',
+            Fragments: {
+                MinimumFeeNotMet: 'min relay fee not met',
             },
-            pubKeyHash: 0x6f,
-            scriptHash: 0xc4,
-            wif: 0xef
+        },
+        Messages: {
+            Wallet: {
+                Created: 'Your wallet has been created and saved!',
+                Mnemonic: 'Store this sequence safely',
+                Failed: 'A wallet could not be created at this moment',
+            },
+            Transactions: {
+                NOTSent: 'Transaction could not be sent',
+                Sent: 'Your transaction was sent'
+            },
+            Errors: {
+                FeeNotMet: 'A fee to process this transaction was not provided'
+            }
         }
     };
 
-    private isOptions = typeforce.maybe(typeforce.compile({
-        compressed: types.maybe(types.Boolean),
-        network: types.maybe(types.Network)
-    }));
+    public static generateMnemonic():string {
+        return bip39.generateMnemonic();
+    }
+
+    public static create(name:string, mnemonic:string):Wallet {
+        const seed = bip39.mnemonicToSeed(mnemonic);
+        const master = bitcoin.HDNode.fromSeedBuffer(seed, Wallet.DEFAULT_NETWORK);
+        const derived = master.derivePath({
+            Encryption: Wallet.DEFAULT_ENCRYPTION,
+            Path: Wallet.DEFAULT_PATH,
+            DBFileName: Wallet.DEFAULT_DB,
+        });
+        const address = derived.getAddress();
+        const wif = derived.keyPair.toWIF();
+        return new Wallet(name, address, wif, Wallet.DEFAULT_NETWORK);
+
+    }
+
+    constructor(name:string, address:string, wif:string, network:Network, password:string = undefined) {
+        this._name = name;
+        this._address = address;
+        this._wif = wif;
+        this._network = network;
+        this._password = password;
+        //this.__utxos = [];
+
+    }
 
     protected matches(password) {
         return password === this._password;
     }
 
-    protected fromWIF(string, network) {
-        const decoded = wif.decode(string)
-        const version = decoded.version
-
-        // list of networks?
-        if (network instanceof Array) {
-            network = network.filter(function (x) {
-                return version === x.wif
-            }).pop()
-
-            if (!network) throw new Error('Unknown network version')
-
-            // otherwise, assume a network object (or default to bitcoin)
-        } else {
-            network = network || Wallet.DEFAULT_NETWORK.bitcoin
-
-            if (version !== network.wif) throw new Error('Invalid network version')
-        }
-
-        return this.fromPrivateKey(decoded.privateKey, {
-            compressed: decoded.compressed,
-            network: network
-        })
+    get password():string {
+        return this._password;
     }
 
-    protected fromPrivateKey(buffer, options) {
-        typeforce(types.Buffer256bit, buffer)
-        if (!ecc.isPrivate(buffer)) throw new TypeError('Private key not in range [1, n)')
-        typeforce(this.isOptions, options)
+    get wif():string {
+        return this._wif;
+    }
 
-        return new ECPair(buffer, null, options)
+    get name():string {
+        return this._name;
+    }
+
+    get address():string {
+        return this._address;
+    }
+
+    get network():Network {
+        return this._network;
+    }
+
+    get utxos():Array<any> {
+        return this._utxos;
     }
 
     /**
      * This is irreversible as there is not way to decrypt the wallet for good.
      * The only way to read the key is with the readDecrypted function
-     * @param password Cleartext or hashed makes no difference
-     * @returns {Wallet} It returns itself
-     * @code const wallet = Wallet.create(name, mnemonic).encrypt(password);
      */
-    public encrypt(password) {
+    public encrypt(password:string):Wallet {
+        if (this.password) throw new Error('Cannot re-encrypt an encrypted key');
         this._password = password;
-        if (this._wif) throw new Error('Cannot re-encrypt an encrypted key');
         const cipher = crypto.createCipher(Wallet.DEFAULT_ENCRYPTION, password);
         this._wif = cipher.update(this._wif, 'utf8', 'hex') + cipher.final('hex');
         return this;
@@ -117,13 +166,39 @@ export class Wallet {
     /**
      * This method will NOT decrypt the wallet but temporarily the key and return it to the calling code
      * This method is NOT symmetrical with the encrypt one.
-     * @param password Hashed or not it will be used, it only needs to match the one used in encryption
-     * @returns {string} It will not return the wallet itself like the encrypt
      */
-    public readDecrypted(password) {
-        if (!this._password) throw new Error('Cannot de-encrypt an key that was not encrypted');
+    public readDecrypted(password:string):string {
+        if (!this.password) throw new Error('Cannot de-encrypt an key that was not encrypted');
         if (!password || !this.matches(password)) throw new Error('Passwords do not match');
         const cipher = crypto.createDecipher(Wallet.DEFAULT_ENCRYPTION, password);
         return cipher.update(this._wif, 'hex', 'utf8') + cipher.final('utf8');
+    }
+
+    public send(btc:number, address:string, fee:number, password:string): string {
+        const satoshis = Math.round(btc * Wallet.DEFAULTS.Bitcoin.Satoshis);
+        const network:Network = Wallet.DEFAULT_NETWORK;
+        const txb = new bitcoin.TransactionBuilder(network);
+
+        let current = 0;
+        for (const utx of this.utxos) {
+
+            txb.addInput(utx.tx_hash_big_endian, utx.tx_output_n);
+
+            current += utx.value;
+            if (current >= (satoshis + fee)) break;
+        }
+
+        txb.addOutput(address, satoshis);
+
+        const change = current - (satoshis + fee);
+        if (change) txb.addOutput(this.address, change);
+
+
+        const wif = this.password ? this.readDecrypted(password) : this.wif;
+        const key = bitcoin.ECPair.fromWIF(wif, network);
+
+        txb.sign(0, key);
+
+        return txb.build().toHex();
     }
 }
